0b12fe96f87524d6a6c70fecad4df283a821a071
Dheeraj Verma <vermadheeraj945@gmail.com>
2025-08-06 20:39:10 +0530

    Corrected cors options

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://ancestropedia.vercel.app/"],
+  origin: ["http://localhost:3000", "https://ancestropedia.vercel.app"],
   credentials: true,
 };
 

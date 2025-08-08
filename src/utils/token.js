6b8dcf41fdab943e98182f3323c96a69d35cae3f
Dheeraj Verma <vermadheeraj945@gmail.com>
2025-08-09 02:44:18 +0530

    added cookies props

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,4 +1,5 @@
 import jwt  from "jsonwebtoken";
+const isProd = process.env.NODE_ENV === "production";
 
 export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_KEY, {
@@ -6,9 +7,11 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    sameSite: "None",
-    secure: true,
-  });
+  httpOnly: true,
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  secure: isProd,
+  sameSite: isProd ? "none" : "lax",
+  domain: isProd ? ".myfrontend.com" : "localhost",
+});
   return token;
 };

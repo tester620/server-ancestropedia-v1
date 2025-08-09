a90c9a5751461da0bc72b1d683064c41ae877a51
Dheeraj Verma <161463894+DheerajVerma945@users.noreply.github.com>
2025-08-09 09:54:12 +0530

    Update token.js

--- src/utils/token.js
+++ src/utils/token.js
@@ -11,7 +11,7 @@ export const generateToken = (userId, res) => {
   maxAge: 7 * 24 * 60 * 60 * 1000,
   secure: isProd,
   sameSite: isProd ? "none" : "lax",
-  domain: isProd ? ".myfrontend.com" : "localhost",
+  domain: isProd ? "ancestropedia.vercel.app" : "localhost",
 });
   return token;
 };

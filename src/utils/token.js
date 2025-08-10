80327b84768d9d616e3678bb1d8f8825770f1558
Dheeraj Verma <vermadheeraj945@gmail.com>
2025-08-10 19:18:54 +0530

    reverted the domain feild in cookies

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,5 +1,4 @@
 import jwt  from "jsonwebtoken";
-const isProd = process.env.NODE_ENV === "production";
 
 export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_KEY, {
@@ -10,7 +9,6 @@ export const generateToken = (userId, res) => {
   maxAge: 7 * 24 * 60 * 60 * 1000,
   sameSite: "None",
   secure: true,
-  domain: isProd ? "ancestropedia.vercel.app" : "localhost",
 });
   return token;
 };

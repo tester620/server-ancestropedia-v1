6dcf185ad31edb7d01524d7383a5362275cc3f46
tester620 <dheerajverma82664442@gmail.com>
2025-08-21 12:34:14 +0530

    Update token.js

--- src/utils/token.js
+++ src/utils/token.js
@@ -8,6 +8,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     sameSite: "None",
     secure: true,
+    path: "/",
     domain: ".ancestropedia.com", 
   });
   return token;

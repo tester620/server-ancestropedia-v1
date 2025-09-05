75f54271bbb8c15acdbe2d48b7a4fef654b7d081
tester620 <dheerajverma82664442@gmail.com>
2025-09-05 13:05:49 +0530

    Update folder.controller.js

--- src/controllers/folder.controller.js
+++ src/controllers/folder.controller.js
@@ -871,6 +871,40 @@ export const uploadPrivateTextFile = async (req, res) => {
   }
 };
 
+export const editPrivateTextContent = async(req,res)=>{
+  const {content,postId} = req.body;
+  try {
+    if (!mongoose.isValidObjectId(postId)) {
+      return res.status(400).json({ message: "Invalid Post Id" });
+    }
+    const post = await PrivatePost.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    const rootFolder = await PrivateFolder.findOne({ userId: req.user._id });
+    if (post.parentFolderId.toString() !== rootFolder._id.toString()) {
+      const postParentFolder = await PrivateNestedFolder.findById(
+        post.parentFolderId
+      );
+      if (
+        postParentFolder.parentFolderId.toString() !== rootFolder._id.toString()
+      ) {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized- Can't edit someone else post" });
+      }
+    }
+    post.description = content;
+    await post.save();
+    return res
+      .status(200)
+      .json({ message: "Post Updated successfully", data: post });
+  } catch (error) {
+    logger.error("Error in editing the post", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
 export const editPrivatePost = async (req, res) => {
   const { postId, name } = req.body;
   try {

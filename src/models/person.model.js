1f4d1524048a99a5cb514beff3b67799e6168e38
tester620 <dheerajverma82664442@gmail.com>
2025-09-09 17:55:08 +0530

    Update person.model.js

--- src/models/person.model.js
+++ src/models/person.model.js
@@ -2,56 +2,36 @@ import mongoose from "mongoose";
 
 const schemaModel = mongoose.Schema(
   {
-    firstName: {
-      type: String,
-      required: true,
-    },
-    location: {
-      type: String,
-      required:true //because we need to show the location on searchTree page and also it can be used to filter the data when searching for the user's tree
-    },
-    creatorId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
-    profession: {
-      type: String,
-      required: true,
-    },
-    dob: {
-      type: Date,
-      required: true,
-    },
-    dod: {
-      type: Date,
-    },
-    gender: {
-      type: String,
-      enum: ["male", "female", "others"],
-      required: true,
-    },
-    living: {
-      type: Boolean,
-      required: true,
-    },
+    firstName: { type: String, required: true, index: true },
+    lastName: { type: String, required: true, index: true },
+    fullName: { type: String, index: true },
 
-    profileImage: {
-      type: String,
-      default: process.env.DEFAULT_IMAGE,
-    },
-    treeId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Tree",
-    },
+    dob: { type: Date, required: true, index: true },
+    dod: { type: Date },
+
+    placeOfBirth: { type: String, index: true },
+    birthPin: { type: String, index: true },
+
+    location: { type: String, required: true, index: true },
+    profession: { type: String, required: true },
+
+    gender: { type: String, enum: ["male", "female", "others"], required: true },
+    living: { type: Boolean, required: true },
+
+    profileImage: { type: String, default: process.env.DEFAULT_IMAGE },
+
+    creatorId: { type: mongoose.Schema.Types.ObjectId, ref: "User", index: true },
+    treeId: { type: mongoose.Schema.Types.ObjectId, ref: "Tree", index: true },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
+
+schemaModel.pre("save", function (next) {
+  if (this.firstName && this.lastName) {
+    this.fullName = `${this.firstName.trim()} ${this.lastName.trim()}`.toLowerCase();
+  }
+  next();
+});
 
 const Person = mongoose.model("Person", schemaModel);
 

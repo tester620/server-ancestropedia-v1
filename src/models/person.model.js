8761f48b8ff8962c31473bd63b30d26a55a14002
tester620 <dheerajverma82664442@gmail.com>
2025-09-11 22:08:07 +0530

    Update person.model.js

--- src/models/person.model.js
+++ src/models/person.model.js
@@ -6,22 +6,26 @@ const schemaModel = mongoose.Schema(
     lastName: { type: String, required: true, index: true },
     fullName: { type: String, index: true },
 
-    dob: { type: Date, required: true, index: true },
-    dod: { type: Date },
+    birthDate: { type: Date, required: true, index: true },
+    toDate: { type: Date },
 
     placeOfBirth: { type: String, index: true },
     birthPin: { type: String, index: true },
 
-    location: { type: String, required: true, index: true },
-    profession: { type: String, required: true },
+    placeOfBirth: { type: String, required: true },
+    birthPin:{type:Number,required:true, index: true},
+    residence: { type: String, required: true },
+    residencePin:{type:Number,required:true, index: true},
+
+    occupation: { type: String, required: true },
 
     gender: { type: String, enum: ["male", "female", "others"], required: true },
     living: { type: Boolean, required: true },
 
     profileImage: { type: String, default: process.env.DEFAULT_IMAGE },
 
-    creatorId: { type: mongoose.Schema.Types.ObjectId, ref: "User", index: true },
-    treeId: { type: mongoose.Schema.Types.ObjectId, ref: "Tree", index: true },
+    creatorId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    treeId: { type: mongoose.Schema.Types.ObjectId, ref: "Tree", index: true,default:null },
   },
   { timestamps: true }
 );

0fe7423f5657089793f797433df4f19985773a96
tester620 <dheerajverma82664442@gmail.com>
2025-09-23 18:38:45 +0530

    Update person.model.js

--- src/models/person.model.js
+++ src/models/person.model.js
@@ -2,46 +2,45 @@ import mongoose from "mongoose";
 
 const schemaModel = mongoose.Schema(
   {
+    _id: { type: String, required: true },
     firstName: { type: String, required: true },
-    lastName: { type: String, required: true},
-    fullName: { type: String, index: true },
-
+    lastName: { type: String },
     birthDate: { type: String, index: true },
     toDate: { type: String },
-
-    birthCity: { type: String},
+    birthCity: { type: String },
     birthState: { type: String },
-    birthCountry: { type:String},
-
+    birthCountry: { type: String },
     residenceCity: { type: String },
     residenceState: { type: String },
     residenceCountry: { type: String },
-
-    occupation: { type: String},
-    maritalStatus:{type:String},
-    haveKids:{type:Boolean},
-    totalKids:{type:Number},
-    haveSiblings:{type:Boolean},
-    totalSiblings:{type:Number},
-
-
-
-    gender: { type: String, enum: ["male", "female", "others"], required: true },
-    living: { type: Boolean},
-
-    profileImage: { type: String, default: process.env.DEFAULT_IMAGE },
-    editedBy:{type:mongoose.Schema.Types.ObjectId, ref:"Person"},
-    treeId: { type: mongoose.Schema.Types.ObjectId, ref: "Tree", index: true,default:null },
+    occupation: { type: String },
+    maritalStatus: { type: String },
+    childrenCount: { type: Number },
+    community: { type: String },
+    religion: { type: String },
+    spouseCount: { type: Number },
+    father: { type: String, ref: "Person" },
+    mother: { type: String, ref: "Person" },
+    childrens: [{ type: String, ref: "Person" }],
+    spouses: [
+      {
+        spouse: { type: String, ref: "Person" },
+        fromDate: { type: String },
+        toDate: { type: String },
+        status: { type: String, enum: ["married", "divorced", "widowed"] },
+      },
+    ],
+    gender: { type: String, enum: ["male", "female", "others"] },
+    living: { type: Boolean },
+    profileImage: {
+      type: String,
+      default:
+        "https://ik.imagekit.io/ancestor/Frame%2025878.png?updatedAt=1758607287267",
+    },
+    editedBy: { type: String, ref: "Person" },
   },
   { timestamps: true }
 );
-
-schemaModel.pre("save", function (next) {
-  if (this.firstName && this.lastName) {
-    this.fullName = `${this.firstName.trim()} ${this.lastName.trim()}`.toLowerCase();
-  }
-  next();
-});
 
 const Person = mongoose.model("Person", schemaModel);
 

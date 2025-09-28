5879f4d489c4095f412b415b6af6636612697a81
tester620 <dheerajverma82664442@gmail.com>
2025-09-28 13:37:21 +0530

    Update tree.controller.js

--- src/controllers/tree.controller.js
+++ src/controllers/tree.controller.js
@@ -453,7 +453,7 @@ export const unlockTree = async (req, res) => {
   const user = req.user;
 
   try {
-    if ((user.allotedTokens-user.tokens) < 10) {
+    if (user.allotedTokens - user.tokens < 10) {
       return res.status(400).json({ message: "Not enough tokens" });
     }
 
@@ -519,5 +519,401 @@ export const getAccessedTree = async (req, res) => {
     return res.status(500).json({
       message: "Internal Server Error",
     });
+  }
+};
+
+export const buildTree = async (req, res) => {
+  const {
+    fatherDetails,
+    selectedFather,
+    motherDetails,
+    selectedMother,
+    personalDetails,
+    selectedUser,
+    spouseDetails = null,
+    selectedSpouse,
+    kidsDetails = [],
+    selectedKids = [],
+    siblingsDetails = [],
+    selectedSiblings = [],
+    maritalData = null,
+  } = req.body;
+
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    async function processImage(details) {
+      if (
+        details?.profileImage &&
+        details.profileImage.startsWith("data:image")
+      ) {
+        const uploadRes = await imagekit.upload({
+          file: details.profileImage,
+          fileName: `${details.firstName || "profile"}.jpg`,
+        });
+        details.profileImage = uploadRes.url;
+      }
+      return details;
+    }
+
+    let user;
+
+    if (selectedUser) {
+      const existingUser = await Person.findById(selectedUser._id).session(
+        session
+      );
+      if (!existingUser) throw new Error("Selected user not found");
+
+      if (existingUser._id.toString() !== req.user._id) {
+        const oldId = existingUser._id;
+        const cloned = existingUser.toObject();
+        delete cloned._id;
+        user = new Person({ ...cloned, _id: req.user._id });
+        await user.save({ session });
+
+        await Person.updateMany(
+          { father: oldId },
+          { $set: { father: req.user._id } },
+          { session }
+        );
+        await Person.updateMany(
+          { mother: oldId },
+          { $set: { mother: req.user._id } },
+          { session }
+        );
+        await Person.updateMany(
+          { childrens: oldId },
+          {
+            $addToSet: { childrens: req.user._id },
+            $pull: { childrens: oldId },
+          },
+          { session }
+        );
+        await Person.updateMany(
+          { "spouses.spouse": oldId },
+          {
+            $addToSet: { spouses: { spouse: req.user._id } },
+            $pull: { spouses: { spouse: oldId } },
+          },
+          { session }
+        );
+
+        await Person.findByIdAndDelete(oldId).session(session);
+      } else {
+        user = existingUser;
+      }
+    } else {
+      const processed = await processImage(personalDetails);
+      user = new Person({ _id: req.user._id, ...processed });
+      await user.save({ session });
+    }
+
+    if (maritalData) {
+      user.maritalStatus = maritalData.status || user.maritalStatus;
+      user.spouseCount = maritalData.spouseCount || user.spouseCount;
+      user.childrenCount = maritalData.childrenCount || user.childrenCount;
+    }
+
+    let father = null;
+    if (selectedFather) {
+      father = await Person.findById(selectedFather._id).session(session);
+      if (father && !user.father) {
+        user.father = father._id;
+        if (!father.childrens.includes(user._id))
+          father.childrens.push(user._id);
+        father.haveKids = true;
+        await father.save({ session });
+      }
+    } else if (fatherDetails) {
+      const processed = await processImage(fatherDetails);
+      father = new Person(processed);
+      await father.save({ session });
+      user.father = father._id;
+      father.childrens.push(user._id);
+      father.haveKids = true;
+      await father.save({ session });
+    }
+
+    let mother = null;
+    if (selectedMother) {
+      mother = await Person.findById(selectedMother._id).session(session);
+      if (mother && !user.mother) {
+        user.mother = mother._id;
+        if (!mother.childrens.includes(user._id))
+          mother.childrens.push(user._id);
+        mother.haveKids = true;
+        await mother.save({ session });
+      }
+    } else if (motherDetails) {
+      const processed = await processImage(motherDetails);
+      mother = new Person(processed);
+      await mother.save({ session });
+      user.mother = mother._id;
+      mother.childrens.push(user._id);
+      mother.haveKids = true;
+      await mother.save({ session });
+    }
+
+    if (father && mother) {
+      const fatherHasMother = father.spouses.find(
+        (s) => s.spouse.toString() === mother._id.toString()
+      );
+      const motherHasFather = mother.spouses.find(
+        (s) => s.spouse.toString() === father._id.toString()
+      );
+      if (!fatherHasMother)
+        father.spouses.push({ spouse: mother._id, status: "married" });
+      if (!motherHasFather)
+        mother.spouses.push({ spouse: father._id, status: "married" });
+      await father.save({ session });
+      await mother.save({ session });
+    }
+
+    let spouse = null;
+    if (selectedSpouse) {
+      spouse = await Person.findById(selectedSpouse._id).session(session);
+      if (spouse) {
+        if (
+          !user.spouses.find(
+            (s) => s.spouse.toString() === spouse._id.toString()
+          )
+        ) {
+          user.spouses.push({ spouse: spouse._id, status: "married" });
+        }
+        if (
+          !spouse.spouses.find(
+            (s) => s.spouse.toString() === user._id.toString()
+          )
+        ) {
+          spouse.spouses.push({ spouse: user._id, status: "married" });
+        }
+        await spouse.save({ session });
+      }
+    } else if (spouseDetails) {
+      const processed = await processImage(spouseDetails);
+      spouse = new Person(processed);
+      await spouse.save({ session });
+      user.spouses.push({ spouse: spouse._id, status: "married" });
+      spouse.spouses.push({ spouse: user._id, status: "married" });
+      await spouse.save({ session });
+    }
+
+    if (Array.isArray(selectedKids)) {
+      for (const kidId of selectedKids) {
+        const kid = await Person.findById(kidId._id).session(session);
+        if (kid) {
+          if (!user.childrens.includes(kid._id)) user.childrens.push(kid._id);
+          if (user.gender === "male") {
+            if (!kid.father) kid.father = user._id;
+            if (spouse && spouse.gender === "female" && !kid.mother)
+              kid.mother = spouse._id;
+          } else if (user.gender === "female") {
+            if (!kid.mother) kid.mother = user._id;
+            if (spouse && spouse.gender === "male" && !kid.father)
+              kid.father = spouse._id;
+          }
+          if (spouse && !spouse.childrens.includes(kid._id))
+            spouse.childrens.push(kid._id);
+          await kid.save({ session });
+          if (spouse) await spouse.save({ session });
+        }
+      }
+    }
+
+    if (Array.isArray(kidsDetails)) {
+      for (const kd of kidsDetails) {
+        const processed = await processImage(kd);
+        const kid = new Person(processed);
+        await kid.save({ session });
+        user.childrens.push(kid._id);
+        if (spouse && !spouse.childrens.includes(kid._id))
+          spouse.childrens.push(kid._id);
+        if (user.gender === "male") {
+          kid.father = user._id;
+          if (spouse && spouse.gender === "female" && !kid.mother)
+            kid.mother = spouse._id;
+        } else if (user.gender === "female") {
+          kid.mother = user._id;
+          if (spouse && spouse.gender === "male" && !kid.father)
+            kid.father = spouse._id;
+        }
+        await kid.save({ session });
+        if (spouse) await spouse.save({ session });
+      }
+    }
+
+    if (Array.isArray(selectedSiblings)) {
+      for (const sibId of selectedSiblings) {
+        const sib = await Person.findById(sibId._id).session(session);
+        if (sib && (father || mother)) {
+          if (father && !sib.father) sib.father = father._id;
+          if (mother && !sib.mother) sib.mother = mother._id;
+          if (father && !father.childrens.includes(sib._id))
+            father.childrens.push(sib._id);
+          if (mother && !mother.childrens.includes(sib._id))
+            mother.childrens.push(sib._id);
+          if (father) father.haveKids = true;
+          if (mother) mother.haveKids = true;
+          await sib.save({ session });
+          if (father) await father.save({ session });
+          if (mother) await mother.save({ session });
+        }
+      }
+    }
+
+    if (Array.isArray(siblingsDetails)) {
+      for (const sd of siblingsDetails) {
+        const processed = await processImage(sd);
+        const sib = new Person(processed);
+        await sib.save({ session });
+        if (father) {
+          sib.father = father._id;
+          if (!father.childrens.includes(sib._id))
+            father.childrens.push(sib._id);
+          father.haveKids = true;
+        }
+        if (mother) {
+          sib.mother = mother._id;
+          if (!mother.childrens.includes(sib._id))
+            mother.childrens.push(sib._id);
+          mother.haveKids = true;
+        }
+        await sib.save({ session });
+        if (father) await father.save({ session });
+        if (mother) await mother.save({ session });
+      }
+    }
+
+    if (user.childrens?.length) {
+      user.childrenCount = user.childrens.length;
+      user.haveKids = true;
+    }
+
+    await user.save({ session });
+
+    const getBuiltTree = async (id) => {
+      try {
+        const visited = new Map();
+
+        const populateFlat = async (personId) => {
+          if (!personId) return;
+          const key = personId.toString();
+          if (visited.has(key)) return;
+
+          const person = await Person.findById(personId)
+            .lean()
+            .select("-__v -createdAt -updatedAt");
+
+          if (!person) return;
+          visited.set(key, {
+            ...person,
+            father: person.father,
+            mother: person.mother,
+            childrens: person.childrens,
+            spouses: person.spouses,
+          });
+
+          if (person.father) await populateFlat(person.father);
+          if (person.mother) await populateFlat(person.mother);
+          await Promise.all(
+            (person.childrens || []).map((c) => populateFlat(c))
+          );
+          await Promise.all(
+            (person.spouses || []).map((s) => populateFlat(s?.spouse))
+          );
+        };
+
+        await populateFlat(id);
+
+        const people = Object.fromEntries(visited);
+        const root = people[id];
+
+        const builtTree = {
+          people,
+          tree: {
+            _id: id,
+            father: root?.father || null,
+            mother: root?.mother || null,
+            childrens: root?.childrens || [],
+            spouses: (root?.spouses || []).map((s) => s?.spouse),
+          },
+        };
+
+        return builtTree;
+      } catch (err) {
+        logger.error("Error in getting full family tree", err);
+      }
+    };
+    const newTree = await getBuiltTree(user._id);
+
+    await session.commitTransaction();
+    res.status(200).json({
+      success: true,
+      data: {
+        user,
+        tree: newTree,
+      },
+    });
+  } catch (err) {
+    await session.abortTransaction();
+    res.status(500).json({ success: false, error: err.message });
+  } finally {
+    session.endSession();
+  }
+};
+
+export const getMyFamilyTree = async (req, res) => {
+  try {
+    const id = req.user._id;
+    const visited = new Map();
+
+    const populateFlat = async (personId) => {
+      if (!personId) return;
+      const key = personId.toString();
+      if (visited.has(key)) return;
+
+      const person = await Person.findById(personId)
+        .lean()
+        .select("-__v -createdAt -updatedAt");
+
+      if (!person) return;
+      visited.set(key, {
+        ...person,
+        father: person.father,
+        mother: person.mother,
+        childrens: person.childrens,
+        spouses: person.spouses,
+      });
+
+      if (person.father) await populateFlat(person.father);
+      if (person.mother) await populateFlat(person.mother);
+      await Promise.all((person.childrens || []).map((c) => populateFlat(c)));
+      await Promise.all(
+        (person.spouses || []).map((s) => populateFlat(s?.spouse))
+      );
+    };
+
+    await populateFlat(id);
+
+    const people = Object.fromEntries(visited);
+    const root = people[id];
+
+    return res.status(200).json({
+      message: "Tree fetched successfully",
+      data: {
+        people,
+        tree: {
+          _id: id,
+          father: root?.father || null,
+          mother: root?.mother || null,
+          childrens: root?.childrens || [],
+          spouses: (root?.spouses || []).map((s) => s?.spouse),
+        },
+      },
+    });
+  } catch (err) {
+    logger.error("Error in getting my family tree", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
